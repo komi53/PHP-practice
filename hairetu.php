98d3e6c3494f1eb98c47db7a6709e404ab54e23d
komi53 <kxs53kuro@gmail.com>
2020-10-28 18:02:40 +0900

    hairetu

---
+++ hairetu.php
@@ -0,0 +1,12 @@
+<?php
+  $fruits_mixed = array (
+    'peach',
+    'blueberry',
+    'apple' => 'red',
+    'banana' => 'yellow',
+    'orange' => 'orange',
+  );
+  echo $fruits_mixed[1], PHP_EOL;
+  echo $fruits_mixed['banana'], PHP_EOL;
+  var_dump($fruits_mixed);
+?>
